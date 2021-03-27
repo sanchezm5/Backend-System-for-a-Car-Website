c46a892744b5a6c1665a597f571a40f22add112c
Maria Sanchez <msanchez157@gmail.com>
2021-03-27 17:24:09 -0400

    Edited data.sql file

--- P02-VehiclesAPI/pricing-service/src/main/resources/data.sql
+++ P02-VehiclesAPI/pricing-service/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO price (id, currency, price, vehicle_id) VALUES (1, "USD", 11000, 1);
-INSERT INTO price (id, currency, price, vehicle_id) VALUES (2, "USD", 12000, 2);
-INSERT INTO price (id, currency, price, vehicle_id) VALUES (3, "USD", 13000, 3);
-INSERT INTO price (id, currency, price, vehicle_id) VALUES (4, "USD", 14000, 4);
-INSERT INTO price (id, currency, price, vehicle_id) VALUES (5, "USD", 15000, 5);
+INSERT INTO price (id, currency, price, vehicle_id) VALUES (1, 'USD', 11000, 1);
+INSERT INTO price (id, currency, price, vehicle_id) VALUES (2, 'USD', 12000, 2);
+INSERT INTO price (id, currency, price, vehicle_id) VALUES (3, 'USD', 13000, 3);
+INSERT INTO price (id, currency, price, vehicle_id) VALUES (4, 'USD', 14000, 4);
+INSERT INTO price (id, currency, price, vehicle_id) VALUES (5, 'USD', 15000, 5);
